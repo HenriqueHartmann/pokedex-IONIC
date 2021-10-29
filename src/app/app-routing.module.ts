e019f45917b05a7b7dfe208e6d026f82364ef380
HenriqueHartmann <alemaohartmann@gmail.com>
2021-10-29 16:12:09 -0300

    importing http module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,10 +11,14 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  //{
+  //  path: 'details',
+  //  loadChildren: () => import('./details/details.module').then( m => m.DetailsPageModule)
+  //},
   {
-    path: 'details',
+    path: 'home/details',
     loadChildren: () => import('./details/details.module').then( m => m.DetailsPageModule)
-  },
+  }
 ];
 
 @NgModule({

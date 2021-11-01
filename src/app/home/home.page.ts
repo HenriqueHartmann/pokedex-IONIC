b2e591c5a61aadb6093ffd5d2f1e8dcf50ffad19
HenriqueHartmann <alemaohartmann@gmail.com>
2021-11-01 20:04:09 -0300

    add searchBar and infiniteScroll

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,56 @@
-import { Component } from '@angular/core';
+import { PokemonService } from '../services/pokemon.service';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonInfiniteScroll } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  offset = 0;
+  pokemon = [];
 
-  constructor() {}
+  @ViewChild(IonInfiniteScroll) infinite: IonInfiniteScroll;
 
+  constructor(private pokeService: PokemonService) {}
+
+  ngOnInit() {
+    this.loadPokemon();
+  }
+
+  loadPokemon(loadMore = false, event?) {
+    if (loadMore) {
+      this.offset += 25;
+    }
+
+    this.pokeService.getPokemon(this.offset).subscribe(res => {
+      this.pokemon = [...this.pokemon, ...res];
+
+      if (event) {
+        event.target.complete();
+      }
+
+      // Optional
+      if (this.offset == 125) {
+        this.infinite.disabled = true;
+      }
+    })
+  }
+
+  onSearchChange(e) {
+    let value = e.detail.value;
+
+    if (value == '') {
+      this.offset = 0;
+      this.loadPokemon();
+      return;
+    }
+
+    this.pokeService.findPokemon(value).subscribe(res => {
+      this.pokemon = [res]
+    }, err => {
+      this.pokemon = [];
+    })
+  }
 }
